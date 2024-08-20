83247b285275858cf050354e37b898c74a636f2d
Lucasgarcc <luuccasgarciia@gmail.com>
2024-08-20 13:50:56 -0300

    refactor: :hammer: Modal refatorado com class

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,23 +1,43 @@
-function initModal() {
-  const botaoAbrir = document.querySelector('[data-modal="abrir"]');
-  const botaoFechar = document.querySelector('[data-modal="fechar"]');
-  const containerModal = document.querySelector('[data-modal="container"]');
+class Modal {
+  constructor(open, close, modal) {
+    this.botaoAbrir = document.querySelector(open);
+    this.botaoFechar = document.querySelector(close);
+    this.containerModal = document.querySelector(modal);
 
-  function toggleModal(e) {
-    e.preventDefault();
-    containerModal.classList.toggle("ativo");
+    /* bind this ap callback para 
+    fazer referência ao objeto da classe */
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.clicouForaModal = this.clicouForaModal.bind(this);
+    this.init();
   }
-  botaoAbrir.addEventListener("click", toggleModal);
-  botaoFechar.addEventListener("click", toggleModal);
 
-  function clicouForaModal(e) {
-    if (e.target === this) {
-      toggleModal(e);
+  toggleModal() {
+    this.containerModal.classList.toggle('ativo');
+  }
+
+  eventToggleModal(event) {
+    event.preventDefault();
+    this.toggleModal();
+  }
+
+  clicouForaModal(event) {
+    if (event.target === this.containerModal) {
+      this.toggleModal();
     }
   }
-  if (botaoAbrir && botaoFechar && containerModal) {
-    containerModal.addEventListener("click", clicouForaModal);
+
+  addEventsModal() {
+    this.botaoAbrir.addEventListener('click', this.eventToggleModal);
+    this.botaoFechar.addEventListener('click', this.eventToggleModal);
+    this.containerModal.addEventListener('click', this.clicouForaModal);
+  }
+
+  init() {
+    if (this.botaoAbrir && this.botaoFechar && this.containerModal) {
+      this.addEventsModal();
+    }
+    return this;
   }
 }
 
-export default initModal();
+export default Modal;
