78c92355b1cd79b7c15a8fcb71000650be347bec
Lucasgarcc <luuccasgarciia@gmail.com>
2024-08-21 14:58:06 -0300

    refactor: :hammer: Anima-Numero refatorado com class

--- js/modules/animaNumeros.js
+++ js/modules/animaNumeros.js
@@ -1,40 +1,53 @@
-function initAnimaNumeros() {
-  function animaNumeros() {
-    const numeros = document.querySelectorAll("[data-numero]"); // alvo data
-    numeros.forEach((numero) => {
-      // para percorrer cada numero do nodeList
+class AnimaNumeros {
+  constructor(numero, observerTarget, observerClass) {
+    this.numeros = document.querySelectorAll(numero);
+    this.observerTarget = document.querySelector(observerTarget);
+    this.observerClass = observerClass;
 
-      const total = +numero.innerHTML; // tranforma text em numero com uso do sinal '=+'
-
-      const incremento = Math.floor(total / 100); // incremento de numeros inteiros Math.floor(), ajuda na equação do numeros transformando em inteiros
-
-      let start = 0; // contador
-      const timer = setInterval(() => {
-        // criar os intervalos de tempo entre os numeros + a function anonima
-
-        start += incremento; // incremento contatenado ao start = 0
-        numero.innerText = start; // exibe o numero no html
-
-        if (start > total) {
-          // verifica for maior que 0
-          numero.innerText = total; // evita numeros ultrapassar o numero do total
-          clearInterval(timer); // limpa o intervalo
-        }
-      }, 50 * Math.random()); // cria um atraso aleatorio entre os numeros
-    });
+    // Bind o this do objeto do callback da mutation
+    this.handleMutation = this.handleMutation.bind(this);
+    this.init();
   }
-  const observerTarget = document.querySelector(".numeros");
-  let observer;
-  // função observador para seção numeros
-  function handleMutation(mutation) {
-    if (mutation[0].target.classList.contains("ativo")) {
-      // se existe no target class ativo
-      observer.disconnect();
-      animaNumeros();
+
+  static incrementorNumero(numero) {
+    const total = +numero.innerHTML;
+    const incremento = Math.floor(total / 100);
+    let start = 0;
+    const timer = setInterval(() => {
+      start += incremento;
+      numero.innerText = start;
+      if (start > total) {
+        numero.innerText = total;
+        clearInterval(timer);
+      }
+    }, 50); // Valor fixo para consistência
+  }
+
+  animaNumeros() {
+    this.numeros.forEach((numero) =>
+      this.constructor.incrementorNumero(numero),
+    );
+  }
+
+  // Metodo que ocorre quando a mutação tiver ativa
+  handleMutation(mutation) {
+    if (mutation[0].target.classList.contains(this.observerClass)) {
+      this.observer.disconnect();
+      this.animaNumeros();
     }
   }
-  observer = new MutationObserver(handleMutation);
-  observer.observe(observerTarget, { attributes: true });
+
+  addMutationObserver() {
+    this.observer = new MutationObserver(this.handleMutation);
+    this.observer.observe(this.observerTarget, { attributes: true });
+  }
+
+  init() {
+    if (this.numeros.length && this.observerTarget) {
+      this.addMutationObserver();
+    }
+    return this;
+  }
 }
 
-export default initAnimaNumeros();
+export default AnimaNumeros;
