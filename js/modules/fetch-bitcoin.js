e05474d7f5687d210203c108c1930cffdff38e79
Lucas Gcarcia <99447073+Lucasgarcc@users.noreply.github.com>
2024-08-23 21:52:49 -0300

    Revert "refactor: :hammer: Fetch Bitcoin refatorado"

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,16 +1,16 @@
 async function initFetchBitcoin() {
-  async function fetchBitcoin(url, traget) {
-    fetch(url)
+  async function fetchBitcoin() {
+    fetch("http://blockchain.info/ticker")
       .then((resp) => resp.json())
       .then((data) => {
-        const btcPreco = document.querySelector(traget);
+        const btcPreco = document.querySelector(".btc-preco");
         btcPreco.innerText = (2000 / data.BRL.sell).toFixed(4);
       })
       .catch((error) => {
         console.log(Error(`Erro: ${error} na requisição da API...`));
       });
   }
-  fetchBitcoin('http://blockchain.info/ticker', '.btc-preco');
+  fetchBitcoin();
 }
 
 export default initFetchBitcoin();
