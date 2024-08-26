2f3c564e5369ea708f64cf91c9e1735cd81e22aa
Lucasgarcc <luuccasgarciia@gmail.com>
2024-08-26 09:24:15 -0300

    fix: :bug: Problema resolvido conflitos e refatoração da Fetch Bitcoin

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,18 +1,17 @@
 async function initFetchBitcoin() {
   async function fetchBitcoin() {
-    fetch("http://blockchain.info/ticker")
+    fetch('http://blockchain.info/ticker')
       .then((resp) => resp.json())
       .then((data) => {
-        const btcPreco = document.querySelector(".btc-preco");
+        const btcPreco = document.querySelector('.btc-preco');
         btcPreco.innerText = (2000 / data.BRL.sell).toFixed(4);
       })
       .catch((error) => {
         console.log(Error(`Erro: ${error} na requisição da API...`));
       });
   }
-  <<<<<<< refatorar-fetch-bitcoin
 
-  fetchBitcoin('http://blockchain.info/ticker', '.btc-preco')
+  fetchBitcoin('http://blockchain.info/ticker', '.btc-preco');
   fetchBitcoin();
 }
 
